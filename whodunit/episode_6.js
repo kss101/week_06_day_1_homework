c5555860f000d32ee3b7701958ed38c375d8e084
Kurt S Steffan <kurt.s.steffan@gmail.com>
2020-12-07 17:16:33 +0000

    Episode 6: Mrs White

--- whodunit/episode_6.js
+++ whodunit/episode_6.js
@@ -15,5 +15,5 @@ const declareMurderer = function () {
 }
 
 changeMurderer();
-const verdict = declareMurderer();
+const verdict = declareMurderer(); // The murderer will be Mrs White. The inital declaration of Col. Mustard will be reasigned to Mrs. Green due to the function call to "changeMurderer()". However, that function contains a call to "plotTwist()" which will reasigne the murderer to Mrs. WHite. The "const" on the functions doesn't prevent us from changing their internal variables; it only prevents us from reassigning them to some other function (or variable)
 console.log(verdict);
