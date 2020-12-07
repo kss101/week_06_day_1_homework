6bb73ae0418d9f7cadabb95b40db639c58c3a0a8
Kurt S Steffan <kurt.s.steffan@gmail.com>
2020-12-07 18:33:46 +0000

    Episode 10: Who dun it... where?

--- whodunit/episode_9.js
+++ whodunit/episode_9.js
@@ -2,12 +2,11 @@ let murderer = 'Professor Plum';
 
 if (murderer === 'Professor Plum') {
   let murderer = 'Mrs. Peacock';
-  return murderer;
 }
 
 const declareMurderer = function() {
   return `The murderer is ${murderer}.`;
 }
 
-const verdict = declareMurderer(); // The murderer will be Prof Plum, as the addition of the "let" prefixing the murderer reassignment as part of the IF statement limits its scope to only within the IF statement.
+const verdict = declareMurderer();
 console.log(verdict);
