60976e7f1bf63c24827cad13340dfc76d6353223
Kurt S Steffan <kurt.s.steffan@gmail.com>
2020-12-07 17:35:11 +0000

    Episode 8: The candlestick

--- whodunit/episode_8.js
+++ whodunit/episode_8.js
@@ -30,5 +30,5 @@ const scenario = {
   }
   
   changeScenario();
-  const verdict = declareWeapon();
+  const verdict = declareWeapon(); // The murder weapon will be the candlestick. The call to "changeScenario()", updates the scenario.room to be dining room (since we can change elements inside the original declaration for scenario, despite the use of "const"). The call to plotTwist now takes place and the condition is true, due to the prior reassignment. This in turn allows for the changing of scenario.murderer to be Col. Mustard. Next, the call to unexpectedOutcome() is made and, again, the condition is now true, thus updating scenario.weapon to be "candelstick"! This is all due to the use of const and how it does not stop a change of it's internal values
   console.log(verdict);
