e1471fb29b2c3098265b081b512210caf7b47e2a
Kurt S Steffan <kurt.s.steffan@gmail.com>
2020-12-07 17:00:27 +0000

    Episode 5: The revolver

--- whodunit/episode_5.js
+++ whodunit/episode_5.js
@@ -12,6 +12,6 @@ const scenario = {
     return `The weapon is the ${scenario.weapon}.`;
   }
   
-  changeWeapon('Revolver');
-  const verdict = declareWeapon();
+  changeWeapon('Revolver'); // This will reasign the senario.weapon from 'Candle Stick' to 'Revolver' as, although scenario is declared as a const, whilst we can't change the whole object, we CAN change the values inside it!
+  const verdict = declareWeapon(); // Due to the reasignment above, the return will have 'Revolver' as the scenario.weapon
   console.log(verdict);
