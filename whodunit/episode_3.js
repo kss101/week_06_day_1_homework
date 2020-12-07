33afff17b16c03844101f4f05cc8f0687897706f
Kurt S Steffan <kurt.s.steffan@gmail.com>
2020-12-07 16:44:10 +0000

    Episode 3:Mrs Peacock... Prof Plum

--- whodunit/episode_3.js
+++ whodunit/episode_3.js
@@ -6,7 +6,7 @@ const declareMurderer = function() {
 }
 
 const firstVerdict = declareMurderer();
-console.log('First Verdict: ', firstVerdict);
+console.log('First Verdict: ', firstVerdict); // 1st verdict will be Mrs Peacock as the "let" variable declared at start will be overwritten within the scope of the function and returned.
 
-const secondVerdict = `The murderer is ${murderer}.`;
+const secondVerdict = `The murderer is ${murderer}.`;// 2nd verdict will be Prof Plum as the "let" variable declared at start will superceed the one in the function due to it's more localised scope.
 console.log('Second Verdict: ', secondVerdict);
