1017f220a79f570208112f6ffc561f80fcc5f7b9
Kurt S Steffan <kurt.s.steffan@gmail.com>
2020-12-07 16:54:42 +0000

    Episode 4:Scarlet, Plum, Mustard...  Mrs Peacock

--- whodunit/episode_4.js
+++ whodunit/episode_4.js
@@ -8,5 +8,5 @@ const declareAllSuspects = function() {
 }
 
 const suspects = declareAllSuspects();
-console.log(suspects);
-console.log(`Suspect three is ${suspectThree}.`);
+console.log(suspects); // This will show the suspects return from the function, with the first two as declared at the start, and the 3rd replaced by Col. Mustard, since suspectThree is reassigned in the function.
+console.log(`Suspect three is ${suspectThree}.`); //This will show the original suspectThree value, Mrs. Peacock, as the function's reasignment will have no effect as it is an anonymous function, it is not "hoisted"
